fb62263df5bc5e0602e3897632d2d3d23d3932dc
jscoba <jscoba@gmail.com>
2013-03-21 13:59:31 +0100

    Archivo del banco de sonidos
    Archivo utilizado para generar las interfaces tanto en android como en php

---
+++ bancoprincipal.php
@@ -0,0 +1,62 @@
+<php
+$principal[0]= array(
+  "nombre" => "Ritmo Base 1",
+	"archivo" => "1" ,
+	"imagen" => "1.png",
+);
+
+$principal[1]= array(
+	"nombre" => "Ritmo Base 2",
+	"archivo" => "2" ,
+	"imagen" => "2.png",
+);
+
+$principal[2]= array(
+	"nombre" => "Ritmo Base 3",
+	"archivo" => "3" ,
+	"imagen" => "3.png",
+);
+
+$principal[3]= array(
+	"nombre" => "Melodía 1",
+	"archivo" => "4" ,
+	"imagen" => "4.png",
+);
+
+$principal[4]= array(
+	"nombre" => "Melodía 2",
+	"archivo" => "5" ,
+	"imagen" => "5.png",
+);
+
+$principal[5]= array(
+	"nombre" => "Melodía 3",
+	"archivo" => "6" ,
+	"imagen" => "6.png",
+);
+
+$principal[6]= array(
+	"nombre" => "Bajo Mayor",
+	"archivo" => "7" ,
+	"imagen" => "7.png",
+);
+
+$principal[7]= array(
+	"nombre" => "Bajo Menor",
+	"archivo" => "8" ,
+	"imagen" => "8.png",
+);
+
+$principal[8]= array(
+	"nombre" => "Bajo Continuo Mayor",
+	"archivo" => "9" ,
+	"imagen" => "9.png",
+);
+
+$principal[9]= array(
+	"nombre" => "Bajo Continuo Menor",
+	"archivo" => "10" ,
+	"imagen" => "10.png",
+);
+
+?>
